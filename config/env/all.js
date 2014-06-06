fcc870f305f8cf1ce77c2b3ce5596d2cb2d39b81
khowarizmi <alkhowarizmi169@gmail.com>
2014-06-07 01:30:47 +0800

    更新定时入库时间

--- config/env/all.js
+++ config/env/all.js
@@ -4,7 +4,7 @@ module.exports = {
 	LOG_MAX_SIZE: 5000000, // 5M
 	PERPAGE: 20, //log文件前端显示每页条数
 	STORAGE: {
-		cron: '* * 2 * * *', // 定时入库的时间
+		cron: '0 20 0 * * *', // 定时入库的时间
 		// cron: '40 39 14 * * *', // 定时入库的时间
 		// delay: 5000, // 日志文件写满后入库的延时ms， 半分钟后入库
 		maxProcess: 1, //队列同时处理的文件数量
