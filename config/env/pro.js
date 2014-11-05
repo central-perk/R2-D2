d19876d82ad25c6f830874c233e1b2e08d2397aa
956238702@qq.com <956238702@qq.com>
2014-11-05 18:06:44 +0800

    commit

--- config/env/pro.js
+++ config/env/pro.js
@@ -1,12 +1,10 @@
 module.exports = {
-    APP: {
-        DB: {
-	        host: '10.80.39.112',
-            // host: '127.0.0.1',
-            port: 27017
-        }
+    DB: {
+        name: 'echuandan_logger',
+        host: '127.0.0.1', //
+        port: 27017
     },
     LOGGERFILE: {
         maxSize: 500
-    },
+    }
 };
